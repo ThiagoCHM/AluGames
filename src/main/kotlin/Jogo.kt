eed34f1cef706b4f324bc1826325e5974e977ed3
Thiago C H Moreira <thiagochm@gmail.com>
2024-05-26 01:06:59 -0300

    Definindo Classe Jogo

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,12 @@
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    var descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
+}
